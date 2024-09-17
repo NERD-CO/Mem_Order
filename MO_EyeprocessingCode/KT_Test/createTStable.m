f0d136337baa6800498625955593426a8004a7fc
ktyner3 <ktyner3@yahoo.com>
2024-09-17 13:36:12 -0600

    Created Fix tsTable function

--- MO_EyeprocessingCode/KT_Test/createTStable.m
+++ MO_EyeprocessingCode/KT_Test/createTStable.m
@@ -16,40 +16,43 @@ ttlNum2 = cellfun(@(x) str2double(x), ttlNum1, "UniformOutput", true);
 tsTable = table(transpose(messTT),transpose(ttlNum2),transpose(timeStamp1),...
     'VariableNames',{'ELmessage','TTLid','timeStamp'});
 
+%% Fix tsTable
+tsTable = Fix_MemOrder_tsTable(tsTable,behavioR);
+
 %% Modify tsTable to remove extra TTLs
 responseEvents = [111 112 113 114 115 116];
-tossEntry = nan(length(tsTable.TTLid),1);
-for ii = 1:length(tsTable.TTLid)
-    if ~ismember(responseEvents,tsTable.TTLid(ii,1))
-        tossEntry(ii,1) = 0;
-    else
-        num1 = tsTable.TTLid(ii-1,1);
-        num2 = tsTable.TTLid(ii,1);
-        if ismember(num1,responseEvents) && ismember(num2,responseEvents)
-            tossEntry(ii,1) = 1;
-        else
-            tossEntry(ii,1) = 0;
-        end
-    end
-end
-tossEntry = logical(tossEntry);
-tsTable = tsTable(~tossEntry,:);
+% tossEntry = nan(length(tsTable.TTLid),1);
+% for ii = 1:length(tsTable.TTLid)
+%     if ~ismember(responseEvents,tsTable.TTLid(ii,1))
+%         tossEntry(ii,1) = 0;
+%     else
+%         num1 = tsTable.TTLid(ii-1,1);
+%         num2 = tsTable.TTLid(ii,1);
+%         if ismember(num1,responseEvents) && ismember(num2,responseEvents)
+%             tossEntry(ii,1) = 1;
+%         else
+%             tossEntry(ii,1) = 0;
+%         end
+%     end
+% end
+% tossEntry = logical(tossEntry);
+% tsTable = tsTable(~tossEntry,:);
 
 %% Add trial ID
-taskEVEnts = [11 1 2 3 4 0];
-trialIDtmp = zeros(height(tsTable),1);
-for ttIi = 1:numel(taskEVEnts)
-    TTLIDlocs = find(tsTable.TTLid == taskEVEnts(ttIi));
-    TTLnuMS = 1:numel(TTLIDlocs);
-    trialIDtmp(TTLIDlocs) = TTLnuMS;
-end
-
-responseEvents = [111 112 113 114 115 116];
-[eventLoc,~] = ismember(tsTable.TTLid,responseEvents);
-TTLnuMS = 1:sum(eventLoc);
-trialIDtmp(eventLoc) = TTLnuMS;
-
-tsTable.TrialID = trialIDtmp;
+% taskEVEnts = [11 1 2 3 4 0];
+% trialIDtmp = zeros(height(tsTable),1);
+% for ttIi = 1:numel(taskEVEnts)
+%     TTLIDlocs = find(tsTable.TTLid == taskEVEnts(ttIi));
+%     TTLnuMS = 1:numel(TTLIDlocs);
+%     trialIDtmp(TTLIDlocs) = TTLnuMS;
+% end
+% 
+% responseEvents = [111 112 113 114 115 116];
+% [eventLoc,~] = ismember(tsTable.TTLid,responseEvents);
+% TTLnuMS = 1:sum(eventLoc);
+% trialIDtmp(eventLoc) = TTLnuMS;
+% 
+% tsTable.TrialID = trialIDtmp;
 
 %% Add rows for responses if they don't exist
 if ~any(ismember(tsTable.TTLid,responseEvents))
